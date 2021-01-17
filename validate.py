92a0353722cdf6c0abba10cfca86e9bbc539ecaf
Howuhh <hsehowuhh@gmail.com>
2021-01-18 01:03:56 +0300

    add seed for rollout

--- validate.py
+++ validate.py
@@ -1,9 +1,9 @@
-import uuid
 import gym
+import tqdm
+import uuid
 import torch
 import minerl
 import logging
-import imageio
 
 import numpy as np
 
@@ -15,10 +15,13 @@ from utils import load_model
 
 logging.basicConfig(level=logging.DEBUG)
         
-        
-def rollout(env, policy, max_steps=np.inf, video=False):
+
+def rollout(env, policy, max_steps=np.inf, video=False, seed=None):
     if video:
-        env = gym.wrappers.monitor.Monitor(env, f"videos/{policy.name}/rollout_{str(uuid.uuid4())}.mp4", resume=True)
+        env = gym.wrappers.monitor.Monitor(env, f"videos/{policy.name}/rollout_{str(uuid.uuid4())}", resume=True)
+
+    if seed is not None:
+        env.seed(seed=seed)  # seed for minecraft world generation
 
     obs, done = env.reset(), False
     total_reward, steps = 0.0, 0.0
@@ -35,8 +38,9 @@ def rollout(env, policy, max_steps=np.inf, video=False):
 
 
 def validate_policy(policies_with_envs, n_evals=5, **kwargs):
-    table = [["policy", f"mean reward (N=5, args: {kwargs})", "std"]]
+    table = [["policy", f"mean reward (N={n_evals}, args: {kwargs})", "std"]]
     
+    # TODO: add tqdm
     for policy, env in policies_with_envs:
         policy_rewards = [rollout(env, policy, **kwargs) for _ in range(n_evals)]
         
@@ -46,24 +50,30 @@ def validate_policy(policies_with_envs, n_evals=5, **kwargs):
 
 
 def main_validate():
-    env = FrameSkipWrapper(gym.make("MineRLTreechop-v0"))
+    env = gym.make("MineRLTreechop-v0")
     
+    # policy_env = (
+    #     (load_model("models/model_stack1_BCE_50_1200"), env),
+    #     (load_model("models/model_stack2_BCE_50_1200"), FrameStackWrapper(env, 2)),
+    #     (load_model("models/model_stack4_BCE_50_1200"), FrameStackWrapper(env, 4)),
+    #     (load_model("models/stack1_BCE_50_1200_grey"), GreyScaleWrapper(env)),
+    #     (load_model("models/stack2_BCE_50_1200_grey"), FrameStackWrapper(GreyScaleWrapper(env), 2)),   
+    #     (load_model("models/stack4_BCE_50_1200_grey"), FrameStackWrapper(GreyScaleWrapper(env), 4)), 
+    # )
     policy_env = (
-        (load_model("models/model_stack1_BCE_50_1200"), env),
-        (load_model("models/model_stack2_BCE_50_1200"), FrameStackWrapper(env, 2)),
-        (load_model("models/model_stack4_BCE_50_1200"), FrameStackWrapper(env, 4)),
-        (load_model("models/stack1_BCE_50_1200_grey"), GreyScaleWrapper(env)),
-        (load_model("models/stack2_BCE_50_1200_grey"), FrameStackWrapper(GreyScaleWrapper(env), 2)),   
-        (load_model("models/stack4_BCE_50_1200_grey"), FrameStackWrapper(GreyScaleWrapper(env), 4)), 
+        (load_model("models/stack2_BCE_20_1200_rgb"), FrameSkipWrapper(FrameStackWrapper(env, 2))),
+        (load_model("models/stack2_BCE_20_1200_rgb"), FrameStackWrapper(FrameSkipWrapper(env), 2))
     )
+    
+    with torch.no_grad():
+        validate_policy(policy_env, n_evals=5, max_steps=500, seed=42)
 
-    validate_policy(policy_env, n_evals=5, max_steps=50)
 
 def main():
-    env = FrameSkipWrapper(FrameStackWrapper(GreyScaleWrapper(gym.make("MineRLTreechop-v0")), 4))
+    env = FrameSkipWrapper(FrameStackWrapper(gym.make("MineRLTreechop-v0"), 2))
     env.make_interactive(port=6666, realtime=True)
     
-    model = load_model("models/stack4_BCE_50_1200_grey")
+    model = load_model("models/stack2_BCE_20_1200_rgb")
     
     with torch.no_grad():
         run_reward = rollout(env, model, video=True)
